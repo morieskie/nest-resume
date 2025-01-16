5865e702461b1f39f1eb8133d5f450ef14112ca6
Derick Fynn <dcfynn@icloid.com>
2025-01-16 16:06:06 +0200

    feat(app): Added TypeORM dataSource module
    feat(config): Added MongoDB connection configuration to TypeORM dataSource

---
+++ src/typesorm.config.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mongodb',
+        host: 'localhost',
+        port: 27017,
+        username: 'mongodb',
+        password: 'mongo',
+        database: 'nest-resume',
+        entities: [join(__dirname, '**', '*.entity{.ts,.js}')], 
+        synchronize: true,
+        useUnifiedTopology: true
+      });
+
+      try {
+        return await dataSource.initialize();
+      } catch (error) {
+        console.error('Error during DataSource initialization', error);
+        throw error;
+      }
+    },
+  },
+];
+
+@Module({
+  providers: [...databaseProviders],
+  exports: [...databaseProviders],
+})
+export class DatabaseModule {}
