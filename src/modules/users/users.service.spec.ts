a5bafc9741630ae8297e66b4004577eba62868cb
Derick Fynn <dcfynn@icloid.com>
2025-01-16 21:50:12 +0200

    feat(testing): Finished unit tests for UserController

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 
+const UserRepository = jest.fn().mockImplementation(() => ({
+  createUser: jest.fn(),
+  updataUser: jest.fn(),
+  findOneUser: jest.fn(),
+  deleteUser: jest.fn(),
+}));
+
+const userProviders = [
+  {
+    provide: 'UserRepository',
+    useFactory: () => new UserRepository(),
+  },
+];
+
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [...userProviders, UsersService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
