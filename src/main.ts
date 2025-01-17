16d2b0b53de0a7e6ad29ef2602e13eacf1baa4d8
Derick Fynn <dcfynn@icloid.com>
2025-01-17 12:36:33 +0200

    feat(server): Fixed cors wildcard value

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: '*'
+      origin: ['*']
     }
   });
 
