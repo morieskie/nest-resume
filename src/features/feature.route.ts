0bf19c11d1d695f8a01eed7948e9a5d4d3894844
Derick Fynn <dcfynn@icloid.com>
2025-01-17 15:52:52 +0200

    feat(activator): Removed DB module dependency

--- src/features/feature.route.ts
+++ src/features/feature.route.ts
@@ -3,11 +3,9 @@ import { UsersModule } from 'src/modules/users/users.module';
 import { EducationModule } from 'src/education/education.module';
 import { ExperienceModule } from 'src/experience/experience.module';
 import { ProjectsModule } from 'src/projects/projects.module';
-import { DatabaseModule } from './database.module';
 
 @Module({
   imports: [
-    // DatabaseModule,
     UsersModule,
     // EducationModule,
     //  ExperienceModule,
