256613db08af81404c80f9304dfaf425486f9a99
Derick Fynn <dcfynn@icloid.com>
2025-01-19 12:41:25 +0200

    feat(enabler): Enabled projects module

--- src/features/feature.route.ts
+++ src/features/feature.route.ts
@@ -1,15 +1,15 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersModule } from 'src/modules/users/users.module';
+import { ProjectsModule } from 'src/modules/projects/projects.module';
 // import { EducationModule } from 'src/education/education.module';
 // import { ExperienceModule } from 'src/experience/experience.module';
-// import { ProjectsModule } from 'src/projects/projects.module';
 
 @Module({
   imports: [
     UsersModule,
+    ProjectsModule,
     // EducationModule,
     //  ExperienceModule,
-    //  ProjectsModule
   ],
 })
 export class FeatureRoute implements NestModule {
